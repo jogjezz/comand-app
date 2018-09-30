6f5364cab357effef293514aeb7afc525eaabfdf
fahmi <fahmi@code.id>
2018-09-30 22:27:33 +0700

    update sql

--- src/dumper/dummy.sql
+++ src/dumper/dummy.sql
@@ -1,23 +1,4 @@
---
--- PostgreSQL database dump
---
 
--- Dumped from database version 10.4
--- Dumped by pg_dump version 10.4
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_with_oids = true;
 
 --
 -- Name: account; Type: TABLE; Schema: public; Owner: postgres
