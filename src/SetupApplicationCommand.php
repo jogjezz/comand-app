6dc080e84665ff7192ac3496284f09f627efd2b1
fahmi <fahmi@code.id>
2018-10-02 12:17:44 +0700

    update

--- src/SetupApplicationCommand.php
+++ src/SetupApplicationCommand.php
@@ -69,6 +69,7 @@ class SetupApplicationCommand extends Command
         exec("chmod -R 777 /var/www/html/storage");//        exec("composer dump-autoload -o");
         exec("chmod -R 777 /var/www/html/bootstrap/cache");//        exec("composer dump-autoload -o");
         $this->createEnv();
+        $this->setUpNginx();
     }
 
     protected function createEnv()
@@ -83,6 +84,17 @@ class SetupApplicationCommand extends Command
         file_put_contents("env.php", $fileGetContet);
         chdir("/");
     }
+
+    private function setUpNginx()
+    {
+        $fileGetContet = file_get_contents(__DIR__ . "/dumper/default");
+        $targetDir = "/etc/nginx/sites-enabled/";
+        chdir($targetDir);
+        file_put_contents("default", $fileGetContet);
+        exec("service nginx restart");
+
+    }
+
 
     private function getReposistory()
     {
