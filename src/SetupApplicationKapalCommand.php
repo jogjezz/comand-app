2664e3516d7ee850bcc72f6e21bcf7c505da61ed
fahmi <fahmi@code.id>
2018-10-02 13:48:05 +0700

    update

--- src/SetupApplicationKapalCommand.php
+++ src/SetupApplicationKapalCommand.php
@@ -70,7 +70,6 @@ class SetupApplicationKapalCommand extends Command
         exec("apt-get -y install php-pgsql");
         exec("apt-get -y install php-xml");
         exec("apt-get -y install php-mbstring");
-        exec("apt-get -y install php-dom");
         exec("composer install");
         exec("composer dump-autoload -o");//        exec("composer dump-autoload -o");
         exec("chmod -R 777 /var/www/html/storage");//        exec("composer dump-autoload -o");
