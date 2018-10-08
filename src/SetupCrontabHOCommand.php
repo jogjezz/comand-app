2748dcf9791d05c5cec3749ae080224bfe7213e0
fahmi <fahmi@code.id>
2018-10-08 09:37:34 +0700

    update

---
+++ src/SetupCrontabHOCommand.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: almustafa dekaulitz (sulaimanfahmi@gmail.com)
+ * Date: 08/10/2018
+ * Time: 1:07
+ */
+
+namespace Src\Helper;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SetupCrontabHOCommand extends Command
+{
+    public function configure()
+    {
+        $this->setName('install-crontab-ho')
+            ->setDescription('Install Schduler on HO.')
+            ->setHelp('Generate scheduler for HO');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        exec("corontab " . __DIR__ . "/dumper/crontab-ho", $output, $return);
+        if ($return) {
+            echo "shome thing happen when create scheduler";
+            die();
+        }
+    }
+}
---
+++ src/SetupCrontabKapalCommand.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: almustafa dekaulitz (sulaimanfahmi@gmail.com)
+ * Date: 08/10/2018
+ * Time: 1:07
+ */
+
+namespace Src\Helper;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SetupCrontabKapalCommand extends Command
+{
+    public function configure()
+    {
+        $this->setName('install-crontab-kapal')
+            ->setDescription('Install Schduler on KAPAL.')
+            ->setHelp('Generate scheduler for KAPAL')
+            ->addArgument("kapalId", InputArgument::REQUIRED, "kapal id");
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $kapalId = $input->getArgument("kapalId");
+        $file = file_get_contents(__DIR__ . "/dumper/crontab-kapal");
+        $fileGetContet = str_replace("%KAPALID%", $kapalId, $file);
+        exec("corontab " . $fileGetContet, $output, $return);
+        if ($return) {
+            echo "shome thing happen when create scheduler";
+            die();
+        }
+    }
+}
